89547377e3c6e9f8eabfe892eb783fd379bbb961
Pelikantapeten <mats.simonsson@sweora.se>
2022-04-20 15:33:05 +0000

    Add connection with Google API to worksheet

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Imports
+import gspread
+from google.oauth2.service_account import Credentials
+
+# This was inspired by and borrowed from 
+# Code Instituet Love Sandwiches project
+# https://github.com/Code-Institute-Solutions/love-sandwiches-p4-sourcecode
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('dad_jokes')
+
+jokesTest = SHEET.worksheet('jokes')
+
+allData = jokesTest.get_all_values()
+
+print(allData)
